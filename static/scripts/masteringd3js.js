112e7b7acb88f0b5b66036281ae4f80b67dc599c
lingsin5234 <sinto.005234@gmail.com>
2020-04-11 14:57:35 -0600

    activity 15 added

--- static/scripts/masteringd3js.js
+++ static/scripts/masteringd3js.js
@@ -2,6 +2,8 @@ var svg = d3.select("#chart-area").append("svg")
 	.attr("width", 400)
 	.attr("height", 400);
 
+/* ---- Part of Lesson 2 ---- */
+/*
 var circle = svg.append("circle")
 	.attr("cx", 100)
 	.attr("cy", 250)
@@ -14,3 +16,32 @@ var rect = svg.append("rect")
     .attr("width", 50)
     .attr("height", 20)
     .attr("fill", "red")
+*/
+
+/* ---- Activity: 15 ---- */
+d3.json("/static/data/buildings.json").then(function(data) {
+    console.log(data);
+    data.forEach(function(d) {
+        d.height = +d.height;  // this converts the strings to integers!
+        console.log(typeof(d.height));
+    });
+
+    var buildings = svg.selectAll("rect")
+        .data(data);
+
+    buildings.enter()
+        .append("rect")
+            .attr("x", function(d,i) {
+                return i * 50 + 10;
+            })
+            .attr("y", 0)
+            .attr("width", 40)
+            .attr("height", function(d) {
+                return d.height;
+            })
+            .attr("fill", "grey");
+
+}).catch(function(error){
+    console.log(error);
+});
+
