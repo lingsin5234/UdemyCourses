4dd5756d6c2b5c92aed5967f769598d6df277acb
lingsin5234 <sinto.005234@gmail.com>
2020-04-13 10:22:44 -0600

    updated udemy menu + project page

--- menus.py
+++ menus.py
@@ -4,4 +4,5 @@ from . import views as udemy_vw
 
 
 Menu.add_item("udemy", MenuItem("My Portfolio", url="/", weight=10))
-Menu.add_item("udemy", MenuItem("Mastering D3.js Course", url="/masteringd3js", weight=10))
+Menu.add_item("udemy", MenuItem("My Udemy Courses", reverse(udemy_vw.project_markdown), weight=10))
+Menu.add_item("udemy", MenuItem("Mastering D3.js Course", reverse(udemy_vw.mastering_d3js), weight=10))
